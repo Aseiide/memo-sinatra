cb57e0b853f1e685efb2f9b8ae9b2e79a28280e8
Aseiide <eiseiide@gmail.com>
2021-06-08 16:49:13 +0900

    wip

--- app.rb
+++ app.rb
@@ -2,26 +2,34 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'securerandom'
 
+#JSON形式でtitleとarticleをmemos.jsonに書き出す
+def write_to_json_file(hash)
+  File.open("./json/#{hash['id']}.json", 'w') do |file| 
+    JSON.dump(hash, file)
+  end
+end
+
+def fetch_memos_from_json_file
+  json_files = Dir.glob('json/*').sort_by { |file| File.birthtime(file) }
+  json_files.map { |file| JSON.parse(File.read(file)) }
+end
+
 get '/' do
   redirect("/memos")
 end
 
 get '/memos' do
+  @all_memos = fetch_memos_from_json_file
   erb :top
 end
 
 post '/memos' do
-  @title = params[:title]
-  @article = params[:article]
-  memos = {
+  new_memo = {
     'id' => SecureRandom.uuid, 
-    "title" => @title,
-    "article"=> @article
+    "title" => params[:title],
+    "article"=> params[:article]
   }
-  File.open("./json/memos.json", 'w') do |file| 
-  JSON.dump(memos, file)
-  end
-  erb :top
+  write_to_json_file(new_memo)
 end
 
 get '/memos/new' do
